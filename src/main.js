7639aee5eddb99cca28344ad6379cef7d3ccdcaa
Brian Mock <bmock@janrain.com>
2015-06-02 11:15:36 -0700

    Adds comments; reorganizes code

--- src/main.js
+++ src/main.js
@@ -8,24 +8,38 @@ var path = require('path')
 var fs = require('fs')
 var pkg = require('../package.json')
 
-var JSON_INDENT_SIZE = 2
+var config = {
+    version: pkg.version
+}
 
+var JSON_INDENT_SIZE = 2
+function prettyJson(x) {
+    return JSON.stringify(x, null, JSON_INDENT_SIZE)
+}
+
+// Try to pretty print a JSON string, falling back to the original if there's a
+// parse error.
+function tryPrettyJson(x) {
+    try {
+        return prettyJson(JSON.parse(x))
+    } catch (e) {
+        return x
+    }
+}
+
+// Print error and exit 1 so we can break automated builds and such.
 function die(message) {
     console.error(message)
     process.exit(1)
 }
 
-var args = process.argv.slice(2)
-if (args.length !== 1) {
-    die('Expected one argument: input RAML file')
-}
-var input = args[0]
-
+// Load file from templates/ directory.
 function loadTemplate(x) {
     var f = path.join(__dirname, '..', 'templates', x)
     return fs.readFileSync(f, 'utf-8')
 }
 
+// Flatten RAML's nested hierarchy of traits and resources.
 function flattenHierarchy(root) {
     var title = root.title
     var traits = traitsToObject(root.traits)
@@ -37,6 +51,7 @@ function flattenHierarchy(root) {
     }
 }
 
+// Convert traits from a list of objects to an object.
 function traitsToObject(traits) {
     return _.reduce(traits, function(acc, obj) {
         var key = Object.keys(obj)[0]
@@ -45,6 +60,7 @@ function traitsToObject(traits) {
     }, {})
 }
 
+// Flatten RAML's nested resources into a list of resources.
 function flattenResources(res, traits) {
     var xs = []
     function recur(parents, res) {
@@ -66,6 +82,7 @@ function flattenResources(res, traits) {
     return xs
 }
 
+// Generate example data for a data type.
 function makeExampleFromType(t, name) {
     if (t === "string") {
         return "EXAMPLE: " + name
@@ -75,14 +92,8 @@ function makeExampleFromType(t, name) {
     throw new Error("makeExampleFromType not implemented for type " + t)
 }
 
-function tryPrettyJson(x) {
-    try {
-        return prettyJson(JSON.parse(x))
-    } catch (e) {
-        return x
-    }
-}
-
+// Flatten all the examples for a resource into a list, or generate a JSON body
+// example based on the declared parameters, filling in junk data.
 function makeExamplesOf(obj) {
     if (obj.body) {
         return _.map(_.pluck(_.values(obj.body), 'example'), tryPrettyJson)
@@ -100,10 +111,8 @@ function makeExamplesOf(obj) {
         : undefined
 }
 
-// RAML nests its structure very heavily. This function attempts to pull it
-// apart, but is not very pretty. I'm sorry.
-//
-// Brian Mock (2015-05-29)
+// Flattens the various methods defined on a resource, so we can have a list at
+// the end, making it easy for the template.
 function flattenMethods(methods) {
     return _.map(methods, function(objForMethod) {
         var obj = _.extend({}, objForMethod)
@@ -124,6 +133,72 @@ function flattenMethods(methods) {
     })
 }
 
+// Load all Handlebars helpers and partials.
+function registerHelpersAndPartials() {
+    handlebars.registerHelper('json', function(data) {
+        var out = hljs.highlight('json', prettyJson(data))
+        return new handlebars.SafeString(
+            '<pre class="hljs lang-json"><code>'
+            + out.value
+            + '</code></pre>'
+        )
+    })
+    handlebars.registerHelper('json_from_string', function(data) {
+        if (data === undefined) {
+            return ''
+        }
+        var err = ''
+        try {
+            data = prettyJson(JSON.parse(data))
+        } catch (e) {
+            err = JSON_PARSE_ERROR
+        }
+        var out = hljs.highlight('json', data)
+        return new handlebars.SafeString(
+            err
+            + '<pre class="hljs lang-json"><code>'
+            + out.value
+            + '</code></pre>'
+        )
+    })
+    handlebars.registerHelper('markdown', function(md) {
+        return md ? new handlebars.SafeString(marked(md)) : ''
+    })
+    handlebars.registerHelper('upper_case', function(s) {
+        return s.toUpperCase()
+    })
+    handlebars.registerPartial('resource', RESOURCE)
+    handlebars.registerPartial('table_of_contents', TABLE_OF_CONTENTS)
+    handlebars.registerPartial('style', STYLE)
+}
+
+// Curried form of tap for injecting side-effects into a pipeline.
+function tap(f) {
+    return function(o) {
+        f(o)
+        return o
+    }
+}
+
+// Grab input RAML filename.
+var args = process.argv.slice(2)
+if (args.length !== 1) {
+    die('Expected one argument: input RAML file')
+}
+var input = args[0]
+
+// Load template files.
+var INDEX = loadTemplate('index.handlebars')
+var RESOURCE = loadTemplate('resource.handlebars')
+var TABLE_OF_CONTENTS = loadTemplate('table_of_contents.handlebars')
+var STYLE = loadTemplate('style.css')
+var JSON_PARSE_ERROR = loadTemplate('invalid_json.html')
+var toHtml = handlebars.compile(INDEX, {
+    preventIndent: true
+})
+
+registerHelpersAndPartials()
+
 function write(x) {
     process.stdout.write(x)
 }
@@ -132,82 +207,17 @@ function parseFail(error) {
     die('Error parsing: ' + error)
 }
 
-var config = {
-    version: pkg.version
-}
-
-function set(k, v) {
-    return function(o) {
-        o[k] = v
-        return o
-    }
-}
-
-var INDEX = loadTemplate('index.handlebars')
-var RESOURCE = loadTemplate('resource.handlebars')
-var TABLE_OF_CONTENTS = loadTemplate('table_of_contents.handlebars')
-var STYLE = loadTemplate('style.css')
-var JSON_PARSE_ERROR = loadTemplate('invalid_json.html')
-
-handlebars.registerPartial('resource', RESOURCE)
-handlebars.registerPartial('table_of_contents', TABLE_OF_CONTENTS)
-handlebars.registerPartial('style', STYLE)
-
-handlebars.registerHelper('upper_case', function(s, options) {
-    return s.toUpperCase()
-})
-
-function prettyJson(x) {
-    return JSON.stringify(x, null, JSON_INDENT_SIZE)
-}
-
-handlebars.registerHelper('json', function(data, options) {
-    var out = hljs.highlight('json', prettyJson(data))
-    return new handlebars.SafeString(
-        '<pre class="hljs lang-json"><code>'
-        + out.value
-        + '</code></pre>'
-    )
-})
-
-handlebars.registerHelper('json_from_string', function(data, options) {
-    if (data === undefined) {
-        return ''
-    }
-    var err = ''
-    try {
-        data = prettyJson(JSON.parse(data))
-    } catch (e) {
-        err = JSON_PARSE_ERROR
-    }
-    var out = hljs.highlight('json', data)
-    return new handlebars.SafeString(
-        err
-        + '<pre class="hljs lang-json"><code>'
-        + out.value
-        + '</code></pre>'
-    )
-})
-
-handlebars.registerHelper('markdown', function(md, options) {
-    return md ? new handlebars.SafeString(marked(md)) : ''
-})
-
-var toHtml = handlebars.compile(INDEX, {
-    preventIndent: true
-})
-
+// Ensure that uncaught exceptions are eventually shown in the console.
 function throwLater(e) {
-    setTimeout(function() {
-        throw e
-    }, 0)
+    setTimeout(function() { throw e }, 0)
 }
 
+// Load the RAML and output the HTML.
 raml
     .loadFile(input)
     .catch(parseFail)
     .then(flattenHierarchy)
-    .then(set('config', config))
+    .then(tap(function(obj) { obj.config = config }))
     .then(toHtml)
     .then(write)
     .catch(throwLater)
